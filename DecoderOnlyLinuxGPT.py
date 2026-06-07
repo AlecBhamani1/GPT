f6dab65b2bd647b115dbdc0c887a35f4a8fd78b5
Alec Bhamani <alecbhamani8@gmail.com>
2026-06-07 18:12:20 -0500

    new max new tokens (higher limit)

--- DecoderOnlyLinuxGPT.py
+++ DecoderOnlyLinuxGPT.py
@@ -239,7 +239,7 @@ for iter in range(max_iters):
 
 #generate from the model
 context = torch.zeros((1,1), dtype=torch.long, device=device)
-output = decode(m.generate(context, max_new_tokens=500)[0].tolist())
+output = decode(m.generate(context, max_new_tokens=50000)[0].tolist())
 print(output)
 with open('output.txt', 'w', encoding='utf-8') as f:
     f.write(output)
